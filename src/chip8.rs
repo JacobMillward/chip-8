a9b3300cedd1a84c9ea7b436d3b90a9d11e6e62b
Jacob Millward <jacob@millward.dev>
2020-04-18 23:55:57 +0100

    Add basic windowing and clock handling
    - Use rust_minifb for simple graphics rendering
    - Convert the chip8 gfx buffer to 32bit framebuffer
    - Read and set the keyboard for the chip8 (hardcoded keys at the moment)
    - Add (unused) functions for loading data into memory
    
    Clock speed is currently set to 60Hz

--- src/chip8.rs
+++ src/chip8.rs
@@ -20,7 +20,7 @@ pub struct Chip8 {
     return_stack: ReturnStack,
     timers: Timers,
     gfx: [u8; 64 * 32],
-    keys: [u8; 16]
+    keys: [u8; 16],
 }
 
 impl Chip8 {
@@ -30,18 +30,46 @@ impl Chip8 {
             registers: Registers {
                 v: [0; 16],
                 i: 0,
-                pc: 0x200
+                pc: 0x200,
             },
             return_stack: ReturnStack {
                 stack: [0; 16],
-                sp: 0
+                sp: 0,
             },
             timers: Timers {
                 delay_timer: 0,
-                sound_timer: 0
+                sound_timer: 0,
             },
             gfx: [0; 64 * 32],
-            keys: [0; 16]
+            keys: [0; 16],
         }
+    }
+
+    pub fn load_rom(&mut self, rom: &[u8; 3584]) {
+        self.load_into_memory(rom, 0x200)
+    }
+
+    pub fn load_into_memory(&mut self, data: &[u8], offset_index: u16) {
+        self.memory[offset_index as usize..].clone_from_slice(data)
+    }
+
+    pub fn execute_next_cycle(&mut self) {}
+
+    pub fn update_timers(&mut self) {
+        if self.timers.delay_timer > 0 {
+            self.timers.delay_timer -= 1;
+        }
+
+        if self.timers.sound_timer > 0 {
+            self.timers.delay_timer -= 1;
+        }
+    }
+
+    pub fn set_keys(&mut self, keys: &[u8; 16]) {
+        self.keys.copy_from_slice(keys)
+    }
+
+    pub fn get_display_buffer(&self) -> &[u8; 64 * 32] {
+        &self.gfx
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,92 @@
+extern crate minifb;
+
+use std::time::{Duration, Instant};
 mod chip8;
 use chip8::Chip8;
+use minifb::{Key, KeyRepeat, Window, WindowOptions};
+
+const TARGET_UPDATE_RATE: u64 = 60;
+const SCREEN_WIDTH: usize = 64;
+const SCREEN_HEIGHT: usize = 32;
 
 fn main() {
-    let chip = Chip8::new();
-    println!("Hello, World!");
+    let mut chip = Chip8::new();
+
+    let mut buffer: Vec<u32> = vec![0; SCREEN_WIDTH * SCREEN_HEIGHT];
+
+    let mut window = Window::new("Chip8", 640, 320, WindowOptions::default()).unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    // Limit to max ~60fps
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    let mut last_update = Instant::now();
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        if Instant::now() - last_update >= Duration::from_millis((1 / TARGET_UPDATE_RATE) * 1000)
+        {
+            let raw_keys = window.get_keys_pressed(KeyRepeat::Yes).unwrap();
+            let keys = get_chip8_keys(raw_keys);
+            chip.set_keys(&keys);
+
+            chip.execute_next_cycle();
+
+            chip.update_timers();
+
+            last_update = Instant::now();
+        }
+
+        convert_display_buffer(chip.get_display_buffer(), &mut buffer);
+
+        window
+            .update_with_buffer(&buffer, SCREEN_WIDTH, SCREEN_HEIGHT)
+            .unwrap();
+    }
+}
+
+fn convert_display_buffer(src: &[u8; SCREEN_WIDTH * SCREEN_HEIGHT], dest: &mut Vec<u32>) {
+    for (index, val) in src.into_iter().enumerate() {
+        dest[index] = match val {
+            0 => 0x0,
+            1 => 0xffffff,
+            _ => unreachable!(),
+        };
+    }
+}
+
+fn get_chip8_keys(keys: Vec<Key>) -> [u8; 16] {
+    let mut result: [u8; 16] = [0; 16];
+    let mut last_set_index = 0;
+    for key in keys.iter() {
+        let converted = match key {
+            Key::Key1 => Some(0x1),
+            Key::Key2 => Some(0x2),
+            Key::Key3 => Some(0x3),
+            Key::Key4 => Some(0xC),
+    
+            Key::Q => Some(0x4),
+            Key::W => Some(0x5),
+            Key::E => Some(0x6),
+            Key::R => Some(0xD),
+    
+            Key::A => Some(0x7),
+            Key::S => Some(0x8),
+            Key::D => Some(0x9),
+            Key::F => Some(0xE),
+    
+            Key::Z => Some(0xA),
+            Key::X => Some(0x0),
+            Key::C => Some(0xB),
+            Key::V => Some(0xF),
+            _ => None,
+        };
+
+        if converted.is_some() {
+            result[last_set_index] = converted.unwrap();
+            last_set_index += 1;
+        }
+    }
+
+    result
 }

09e04077107126db76e841e3a943caa8b62435d1
Jacob Millward <jacob@millward.dev>
2022-05-24 21:52:46 +0100

    Add CLI via `clap`

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,24 @@ use std::{
     fs::File,
     io::Read
 };
+use clap::Parser;
+
 mod chip8;
 mod window;
 
 use window::run_chip8_program;
 
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// ROM file to play
+    input_rom: String,
+}
+
 fn main() {
-    let mut file = File::open("data/Pong.ch8").unwrap();
+    let args = Args::parse();
+
+    let mut file = File::open(args.input_rom).unwrap();
     let mut data = Vec::<u8>::new();
     file.read_to_end(&mut data).expect("File not found!");
    

0ef6a30789b708cee623ec8e9983fd5009940bd3
Jacob Millward <jacob@millward.dev>
2022-05-24 21:16:25 +0100

    Refactor and add tests to ReturnStack

--- src/chip8/cpu.rs
+++ src/chip8/cpu.rs
@@ -134,7 +134,7 @@ impl CPU {
             }
             // RET
             (0, 0, 0xE, 0xE) => {
-                self.registers.pc = self.return_stack.pop();
+                self.registers.pc = self.return_stack.pop().unwrap();
             }
             // JP addr
             (1, _, _, _) => {
@@ -143,7 +143,7 @@ impl CPU {
             // CALL addr
             (2, _, _, _) => {
                 self.registers.inc_pc();
-                self.return_stack.push(self.registers.pc);
+                self.return_stack.push(self.registers.pc).expect("Unable to push value to stack");
                 self.registers.pc = nnn;
             }
             // SE Vx, byte
--- src/chip8/return_stack.rs
+++ src/chip8/return_stack.rs
@@ -1,3 +1,11 @@
+#[derive(Debug, PartialEq)]
+pub(super) enum StackError {
+    Overflow,
+    Underflow
+}
+
+
+
 pub(super) struct ReturnStack {
     stack: [u16; 16],
     sp: u8,
@@ -10,21 +18,67 @@ impl ReturnStack {
             sp: 0,
         }
     }
-    pub fn push(&mut self, val: u16) {
+    pub fn push(&mut self, val: u16) -> Result<(), StackError> {
         if self.sp as usize == self.stack.len() - 1 {
-            panic!("Chip8 Stack Overflow");
+            return Err(StackError::Overflow);
         }
 
         self.stack[self.sp as usize] = val;
         self.sp += 1;
+
+        Ok(())
     }
 
-    pub fn pop(&mut self) -> u16 {
+    pub fn pop(&mut self) -> Result<u16, StackError> {
         if self.sp == 0 {
-            panic!("Chip8 Stack Underflow");
+            return Err(StackError::Underflow);
+
         }
 
         self.sp -= 1;
-        self.stack[self.sp as usize]
+        Ok(self.stack[self.sp as usize])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_push_and_retrieve_value() {
+        let mut stack = ReturnStack::new();
+        stack.push(2).expect("Unable to push value to stack");
+        let val = stack.pop().unwrap();
+        assert_eq!(val, 2);
+    }
+
+    #[test]
+    fn does_overflow_after_16_items() {
+        let mut stack = ReturnStack::new();
+        for i in 1..16 {
+            let result = stack.push(i);
+            assert!(result.is_ok());
+        }
+
+        let result = stack.push(1);
+        assert!(result.is_err());
+
+        assert_eq!( match result {
+            Ok(_) => unreachable!(),
+            Err(err) => err
+        }, StackError::Overflow);
+    }
+
+    #[test]
+    fn does_underflow_when_empty() {
+        let mut stack = ReturnStack::new();
+
+        let result = stack.pop();
+        assert!(result.is_err());
+
+        assert_eq!( match result {
+            Ok(_) => unreachable!(),
+            Err(err) => err
+        }, StackError::Underflow);
     }
 }
